1650ecf993b44aa2d9ece3df1c0222ccb43e7ffb
PmGabeto <pedromsgabeto@gmail.com>
2026-05-09 14:38:34 -0300

    add wifi

---
+++ código/src/wifi/webServerManager.cpp
@@ -0,0 +1,48 @@
+﻿#include <WiFi.h>
+#include <WebServer.h>
+#include "WebServerManager.h"
+#include "storage/MemoriaSistema.h"
+
+extern MemoriaSistema memoria;
+
+WebServer server(80);
+
+const char *ssid = "SUA_REDE";
+const char *senha = "SUA_SENHA";
+
+void iniciarWiFi()
+{
+    WiFi.begin(ssid, senha);
+
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("\nWiFi conectado!");
+    Serial.println(WiFi.localIP());
+}
+
+void iniciarServidor()
+{
+
+    server.on("/api/dados", HTTP_GET, []()
+              {
+
+        EstadoSistema estado = memoria.obterEstadoSistema();
+
+        String json = "{";
+        json += "\"temperatura\":" + String(estado.ultimaLeitura.temperatura) + ",";
+        json += "\"umidade\":" + String(estado.ultimaLeitura.umidade);
+        json += "}";
+
+        server.send(200, "application/json", json); });
+
+    server.begin();
+}
+
+void atualizarServidor()
+{
+    server.handleClient();
+}
---
+++ código/src/wifi/webServerManager.h
@@ -0,0 +1,8 @@
+﻿#ifndef WEBSERVERMANAGER_H
+#define WEBSERVERMANAGER_H
+
+void iniciarWiFi();
+void iniciarServidor();
+void atualizarServidor();
+
+#endif
